dbfd5aa7ec2e6dcfe7d4982e86789f93e91c79e1
vishnupriyarmkec <koll20145.cs@rmkec.ac.in>
2023-08-23 22:35:46 +0530

    Hello Pavani

--- server.py
+++ server.py
@@ -5,15 +5,27 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    fn=data["first"]
+    sn=data["second"]
+    res={
+        "result":fn+sn
+    }
+    return res
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    fn=data["first"]
+    sn=data["second"]
+    res={
+        "result":fn+sn
+    }
+    return res
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
